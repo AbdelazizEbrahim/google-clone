f44f5ba8a88f694c1d71aebd60f993c2782de667
Abdelaziz Ebrahim <abdelazizebrahim5980@gmail.com>
2024-11-30 00:03:50 +0300

    Search results listed with link

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -11,7 +11,7 @@ export default async function WebSearchPage({ searchParams }) {
   await new Promise((resolve) => setTimeout(resolve, 2000));
 
   const response = await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchTerm}&start=${start}`,
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchTerm}&start=${start}`,
     { next: { revalidate: 0 } }
   );
 
